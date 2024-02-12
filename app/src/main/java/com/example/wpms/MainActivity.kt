de4480db8c0859b8c831b50eb82eb8b0f0953783
Ernesto Sierra <105014176+FleeboG@users.noreply.github.com>
2024-02-11 23:46:50 -0600

    MainActivity.kt

--- app/src/main/java/com/example/wpms/MainActivity.kt
+++ app/src/main/java/com/example/wpms/MainActivity.kt
@@ -6,17 +6,6 @@ package com.example.wpms
 // Once connection is establish, read the data being sent
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.wpms.ui.theme.WPMSTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.net.Socket
@@ -24,29 +13,41 @@ import kotlinx.coroutines.*
 import android.widget.Toast
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("Main Activity", "on create call")
-        setContent {
-            WPMSTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<View>(R.id.textView1) as TextView
+        val textView2 = findViewById<View>(R.id.textView2) as TextView
+        val textView3 = findViewById<View>(R.id.textView3) as TextView
+        val textView4 = findViewById<View>(R.id.textView4) as TextView
+        val textView5 = findViewById<View>(R.id.textView5) as TextView
+        val textView6 = findViewById<View>(R.id.textView6) as TextView
 
+        // Initialization variables to store TCP random pressure and moisture values
+        var pressureOneVal = ""
+        var pressureTwoVal = ""
+        var pressureThreeVal = ""
+        var moistureVal = ""
+        val pressureThreshold = 65
 
-        // This allows for things to happen in the background of the app (Without this app keeps crashing on itself)
+        // Works on the background off the app (If we dont have this app keeps crashing)
         CoroutineScope(Dispatchers.IO).launch {
 
-            // Calls the clientTCP function to connect to server
-            clientTCP()
+            // Calls TCP server connection
+            val dataList = clientTCP()
+
+            // Store the returned data
+            pressureOneVal =  " ${dataList.getOrNull(0)}"
+            pressureTwoVal =  " ${dataList.getOrNull(1)}"
+            pressureThreeVal =  " ${dataList.getOrNull(2)}"
+            moistureVal =  " ${dataList.getOrNull(3)}"
 
             withContext(Dispatchers.Main) {
                 Toast.makeText(applicationContext, "Network operation completed", Toast.LENGTH_LONG).show()
@@ -54,10 +55,35 @@ class MainActivity : ComponentActivity() {
 
         }
 
+        //delay method
+        val runnable = Runnable {
+            textView2.text = "Pressure level on sensor one: " + pressureOneVal
+            textView3.text = "Pressure level on sensor two:" + pressureTwoVal
+            textView4.text = "Pressure level on sensor three:" + pressureThreeVal
+            if(pressureOneVal.trim().toInt() > pressureThreshold || pressureTwoVal.trim().toInt() > pressureThreshold || pressureThreeVal.trim().toInt() > pressureThreshold) {
+                textView5.text = "Pressure Levels Exceede Threshold!"
+            } else {
+                textView5.text = "Pressure Levels are Okay!"
+            }
+            if(moistureVal == "1") {
+                textView6.text = "Moissture Detected: False"
+            } else {
+                textView6.text = "Moissture Detected: True"
+            }
+            textView.text = "Reading Done."
+        }
+
+        //setting up parameters for the delay method
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(runnable, 15000)
+        textView.text = "Reading Please Wait..."
+
     }
+
+
 }
 
-fun clientTCP() {
+fun clientTCP(): List<String> {
     // Define the server address and port
     val serverAddress = "10.0.2.2"
     val port = 12345
@@ -84,47 +110,17 @@ fun clientTCP() {
         val data3 = reader.readLine()
         println("Received data from server: $data3")
 
-        // Read and print the fourth line of data from the server
+        // Read and print the third line of data from the server
         val data4 = reader.readLine()
-        if(data4 == "1")
-        {
-            println("Moisture Detected")
-        } else {
-            println("Moisture Not Detected")
-        }
-
+        println("Received data from server: $data4")
 
         // Close the socket
         socket.close()
+
+        return listOf(data1, data2, data3, data4)
     } catch (e: Exception) {
         // Handle any exceptions
         e.printStackTrace()
-    }
-}
-
-// private fun updateUI(data: String) {
-//        // Parse the received data (pressure and moisture readings)
-//        val readings = data.split("|")
-//        val pressureReading = readings[0].toDouble()
-////        val moistureReading = readings[1].toDouble()
-//
-//        // Update UI with the received readings
-//        // For example, update TextViews with pressure and moisture readings
-////        pressureTextView.text = "Pressure: $pressureReading"
-////        moistureTextView.text = "Moisture: $moistureReading"
-//}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WPMSTheme {
-        Greeting("Android")
+        return emptyList()
     }
 }
