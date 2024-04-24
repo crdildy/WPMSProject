0b702cafee401664978e6a3cfbbee74be6b16d3d
cphinric <parkerhinrichs914@gmail.com>
2024-04-24 15:29:07 -0500

    Worked on datbase implemntation with user

--- app/src/main/java/com/example/wpms/Model/User.kt
+++ app/src/main/java/com/example/wpms/Model/User.kt
@@ -3,5 +3,6 @@ package com.example.wpms.Model
 data class User(
     val userId: String = "", // Unique identifier for the user (e.g., Firebase user ID)
     val name: String = "", // Name of the user
-    val role: String = "" // Role of user as either caregiver or patient
+    val role: String = "", // Role of user as either caregiver or patient
+    val profilePictureUrl: String = "" // URL of the profile picture
 )
